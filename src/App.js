7792ed807ebc35fb7358ec22a0f8c79c0e29ebd4
Yurii Kazakov <gfnvortex@gmail.com>
2023-11-09 17:01:41 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React, {useState} from "react";
 import './App.css';
 
+
 function App() {
+    const [counter, setCounter] = useState(0);
+
+    function increment() {
+        setCounter(counter + 1)
+    }
+
+    function decrement() {
+        setCounter(counter - 1)
+    }
+
     return (
         <>
-            <button>Just a button</button>
+            <h1>Счетчик: {counter}</h1>
+            <button onClick={increment} className="btn btn-success">Increase</button>
+            <button onClick={decrement} className="btn btn-danger">Decrease</button>
         </>
     );
 }
